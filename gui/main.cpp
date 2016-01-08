fd71e08274f4d2ab463d16a59c5ffb363d80f24c
Icenowy Zheng <icenowy@outlook.com>
2016-01-08 23:22:35 +0800

    gui: add board-size calculation

--- gui/main.cpp
+++ gui/main.cpp
@@ -12,8 +12,12 @@ void init_sdl ()
 }
 
 #define WINDOW_TITLE "Hexakis"
-#define WINDOW_HEIGHT 512
-#define WINDOW_WIDTH 512
+#define BOARD_HEIGHT 40
+#define BOARD_WIDTH 20
+#define BOARD_TILE_SIZE 16
+
+#define WINDOW_HEIGHT (BOARD_HEIGHT*BOARD_TILE_SIZE)
+#define WINDOW_WIDTH (BOARD_WIDTH*BOARD_TILE_SIZE)
 
 SDL_Window *mainWindow;
 
@@ -21,7 +25,7 @@ void sdl_create_window ()
 {
 	mainWindow = SDL_CreateWindow (WINDOW_TITLE,
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		WINDOW_HEIGHT, WINDOW_WIDTH, SDL_WINDOW_ALLOW_HIGHDPI);
+		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 	if (!mainWindow) {
 		std::cerr << "Unable to create main window." << std::endl;
 		std::exit (2);
